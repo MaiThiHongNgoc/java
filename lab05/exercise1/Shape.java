dbde27234de02fbebff4e5c1833c4c3f57b7af38
MaiThiNgoc <ngoc72887@gmail.com>
2024-04-04 20:58:21 +0700

    hnupdate

---
+++ lab05/exercise1/Shape.java
@@ -0,0 +1,14 @@
+package lab05.exercise1;
+
+public abstract class Shape {
+    private String color;
+
+    public Shape() {
+    }
+    abstract double getArea();
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ lab05/exercise2/Movable.java
@@ -0,0 +1,8 @@
+package lab05.exercise2;
+
+interface Movable{
+    void moveUp();
+    void moveDown();
+    void moveLeft();
+    void moveRight();
+}
